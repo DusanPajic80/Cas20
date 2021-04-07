a0022cbe4498c3a69cc24e3d187a6e777300a146
Skola Rajak QA <skolarajak.qa@gmail.com>
2021-04-07 23:00:54 +0200

    Add project files.

---
+++ Cas20/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Cas20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Ovo je moj prvi program
+             * viselinijski komentar */
+            Console.WriteLine("Zdravo svete.");
+
+            // Ovo je jednolinijski komentar
+            Console.ReadKey();
+        }
+    }
+}
