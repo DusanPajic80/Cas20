78931c8f94c68f2aef06753a1056e99f500784bd
Dusan Pajic <dusanpajic80@gmail.com>
2021-04-14 17:00:00 +0200

    Dodat unos omiljenog broja i kvadrat omiljenog broja

--- Cas20/Program.cs
+++ Cas20/Program.cs
@@ -8,7 +8,14 @@ namespace Cas20
         {
             /* Ovo je moj prvi program
              * viselinijski komentar */
-            Console.WriteLine("Zdravo svete.");
+             Console.WriteLine("koji je tvoj omiljeni broj?");
+
+            int mojBroj = Convert.ToInt32(Console.ReadLine());
+            
+           
+
+            Console.WriteLine("Kvadrat tvog omiljenog broja je  " + mojBroj * mojBroj);
+
 
             // Ovo je jednolinijski komentar
             Console.ReadKey();
